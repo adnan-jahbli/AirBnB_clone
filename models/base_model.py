06d98375ef2ce03080b360c85159bd6858277cff
adnan-jahbli <adnan.jahbli@gmail.com>
2023-08-10 21:12:50 +0000

    creating the engine package and the file_storage module and updating the __init__.py of models

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@
 import uuid
 import datetime
 import time
+from models import storage
 
 
 class BaseModel:
@@ -40,6 +41,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.datetime.now()
             self.updated_at = datetime.datetime.now()
+            storage.new(self)
 
     def save(self):
         """
@@ -47,6 +49,7 @@ class BaseModel:
         an update.
         """
         self.updated_at = datetime.datetime.now()
+        storage.save()
 
     def to_dict(self):
         """
---
+++ models/engine/file_storage.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+""" A class that serializes instances to a JSON file and
+deserializes JSON file to instances: """
+import json
+
+
+class FileStorage:
+    """
+    This class manages data storage and retrieval in JSON format.
+
+    Private class attributes:
+    - __file_path: The file path for JSON storage.
+    - __objects: A dictionary to store objects.
+
+    Public methods:
+    - all(self): Returns the dictionary of all stored objects.
+    - new(self, obj): Adds a new object to the storage dictionary.
+    - save(self): Saves the current objects to the JSON file.
+    - reload(self): Reloads objects from the JSON file.
+
+    Example usage:
+    ```
+    storage = FileStorage()
+    data = {"id": "123", "__class__": "Example"}
+    storage.new(data)
+    storage.save()
+    storage.reload()
+    all_objects = storage.all()
+    ```
+    """
+
+    __file_path = "./file.json"  # Private attribute for JSON file path
+    __objects = {}               # Private attribute to store objects
+
+    def all(self):
+        """
+        Returns:
+        A dictionary containing all stored objects.
+        """
+        return FileStorage.__objects
+
+    def new(self, obj):
+        """
+        Adds a new object to the storage dictionary.
+
+        Args:
+        - obj: A dictionary representing the object to be added.
+        """
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        FileStorage.__objects[key] = obj.to_dict()
+
+    def save(self):
+        """
+        Saves the current objects to the JSON file.
+        """
+        with open(FileStorage.__file_path, 'w') as json_file:
+            json.dump(FileStorage.__objects, json_file)
+
+    def reload(self):
+        """
+        Reloads objects from the JSON file.
+        """
+        try:
+            with open(FileStorage.__file_path, 'r') as json_file:
+                FileStorage.__objects = json.load(json_file).copy()
+        except FileNotFoundError:
+            pass
