8ee4b3a742062b5220606b8020a870c3a211fb7a
adnan-jahbli <adnan.jahbli@gmail.com>
2023-08-12 19:39:26 +0000

    modefying the position of the import of storage instance

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,6 @@
 import uuid
 import datetime
 import time
-from models import storage
 
 
 class BaseModel:
@@ -38,6 +37,7 @@ class BaseModel:
                 elif key != "__class__":
                     setattr(self, key, value)
         else:
+            from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.datetime.now()
             self.updated_at = datetime.datetime.now()
@@ -48,6 +48,7 @@ class BaseModel:
         Updates the 'updated_at' timestamp to the current time, indicating
         an update.
         """
+        from models import storage
         self.updated_at = datetime.datetime.now()
         storage.save()
 
