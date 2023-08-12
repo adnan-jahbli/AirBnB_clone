5abf8d263d26c7d30d8ebfbc2289f3623175d988
adnan-jahbli <adnan.jahbli@gmail.com>
2023-08-12 22:06:13 +0000

    updating base_model by changing position of importing storage instance to top

--- models/base_model.py
+++ models/base_model.py
@@ -6,6 +6,7 @@ class for the AirBnb clone console.
 import uuid
 import datetime
 import time
+from models import storage
 
 
 class BaseModel:
@@ -34,7 +35,6 @@ class BaseModel:
                 elif key != "__class__":
                     setattr(self, key, value)
         else:
-            from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.datetime.now()
             self.updated_at = datetime.datetime.now()
@@ -45,7 +45,6 @@ class BaseModel:
         Updates the 'updated_at' timestamp to the current time, indicating
         an update.
         """
-        from models import storage
         self.updated_at = datetime.datetime.now()
         storage.save()
 
